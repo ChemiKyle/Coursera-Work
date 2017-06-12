dbd366381a0657386d79128cf078d8d647316790
kyle <kyle.2493@gmail.com>
2017-06-12 18:25:42 -0400

    Migrated to singular repo for all Coursera work

---
+++ README.md
@@ -0,0 +1,5 @@
+# Machine Learning Assignments
+
+Assignments from my 2017 run through Dr. Andrew Ng's Machine Learning class hosted on Coursera. Dumping them here so I can work between computers.
+
+Assignments are done with Octave v 4.0.3
---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,39 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+% Problem can be solved with vectorization, may be size dependent like Normal
+use_vector_method = 0;
+if m < 10000
+  use_vector_method = 1;
+end
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta.
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+    if logical(use_vector_method)
+      d_J_d_theta = ((((X*theta) - y)')*X)';
+      theta = theta - (1/m)*(alpha)*d_J_d_theta;
+    else
+      % TODO: iterative implementation for arbitrarily sized theta
+    end
+    % ============================================================
+
+    % Save the cost J in every iteration
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
